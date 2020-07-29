f25e2c71775b92393cd74cf8b6034ca437b93685
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-28 22:24:47 -0400

    finished

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,23 @@
 package com.stayready;
 
 public class Problem03 {
+
+    // determines minimum bribes without element up more than 2 indexes
+    public String minimumBribes(int[] q) {
+        Integer bribes = 0;
+        for (int i = 0; i < q.length; i++) {
+            if (q[i] - (i + 1) > 2) {
+                return "Too Chaotic";
+            }
+            // nested for loop compares all elements up to i index to the specific element at i;
+            for (int j = 0; j < i; j++) {
+                if (q[j] > q[i]) {
+                    bribes++;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(bribes);
+        return bribes.toString();
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,62 @@
 package com.stayready;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class Problem03Test {
+    Problem03 obj = new Problem03();
+
+    @Test
+    public void minimumBribesTest() {
+        int[] testArr = {2, 1, 5, 3, 4};
+
+        String expected = "3";
+
+        assertEquals(expected, obj.minimumBribes(testArr));
+    }
+
+    @Test
+    public void minimumBribesTest2() {
+        int[] testArr = {2, 5, 1, 3, 4};
+
+        String expected = "Too Chaotic";
+
+        assertEquals(expected, obj.minimumBribes(testArr));
+    }
+
+    @Test
+    public void minimumBribesTest3() {
+        int[] testArr = {5, 1, 2, 3, 7, 8, 6, 4};
+
+        String expected = "Too Chaotic";
+
+        assertEquals(expected, obj.minimumBribes(testArr));
+    }
+
+    @Test
+    public void minimumBribesTest4() {
+        // 1 2 3 4 5 6 7 8
+        // 5 made two bribes, 7 made two bribes, 8 made 2 bribe, 6 made 1 bribe
+        // 1 2 3 5 4 6 7 8 - 1
+        // 1 2 5 3 4 6 7 8 - 2
+        // 1 2 5 3 4 7 6 8 - 3
+        // 1 2 5 3 7 4 6 8 - 4
+        // 1 2 5 3 7 4 8 6 - 5
+        // 1 2 5 3 7 8 4 6 - 6
+        // 1 2 5 3 7 8 6 4 - 7
+        int[] testArr = {1, 2, 5, 3, 7, 8, 6, 4};
+
+        String expected = "7";
+
+        assertEquals(expected, obj.minimumBribes(testArr));
+    }
+
+    @Test
+    public void minimumBribesTest5() {
+        int[] testArr = {1, 2, 5, 3, 4, 7, 8, 6};
+
+        String expected = "4";
+
+        assertEquals(expected, obj.minimumBribes(testArr));
+    }
 }
