87b1bae813722a4c4f63af03ab9c1d856bf0347f
Juliana Paul <paul.juliana15@gmail.com>
2020-07-29 00:17:45 -0400

    Completed

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,44 @@
 package com.stayready;
 
 public class Problem03 {
+
+    /**
+     * Partner: Josiah
+     * 
+     * Step 1: Creates a method that takes in a array
+     * Step 2: Iterate through given array
+     * Step 3: In each iteration check to see if index value is greater than index position
+     * Step 4: If greater than 2 then "Too chaotic"
+     * Step 5: ...
+     * 
+     */
+
+    static int minimumBribes(int[] givenArray) {
+
+        int bribes = 0;
+
+        for(int i = givenArray.length - 1; i >= 0; i--)
+        {
+            int currentPosition  = givenArray[i] - (i + 1);
+            
+            if(currentPosition > 2)
+            { 
+                bribes = 404; //assuming that bribes is min 3 or greater
+                System.out.println("CHAOTIC");
+                break; 
+            }
+            else{
+               int compare = Math.max(0, givenArray[i]-2);
+                
+                for(int j = compare; j <i; j++)
+                {
+                    if(givenArray[j] > givenArray[i]) 
+                    {
+                        bribes++;
+                    }
+                }
+            }
+        }
+        return bribes;
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,38 @@
 package com.stayready;
 
-public class Problem03Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem03Test{
+
+    
+    @Test
+    public void minimumBribesTest1(){
+        int[] given = {1, 2, 4, 3, 5};
+        int expected = 1;
+
+        int actual = Problem03.minimumBribes(given);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void minimumBribesTest2(){
+        int[] given = {1, 2, 3, 4, 5};
+        int expected = 0;
+
+        int actual = Problem03.minimumBribes(given);
+
+        Assert.assertEquals(actual, expected);
+    }
+    
+    @Test
+    public void minimumBribesTest3(){
+        int[] given = {5, 1, 2, 3, 4};
+        int expected = 404;
+
+        int actual = Problem03.minimumBribes(given);
+
+        Assert.assertEquals(actual, expected);
+    }
 }
