719a88760b44877dcccd76462ceca0ab0f1fcbbd
Joshua Bowman <joshuabowman@Joshuas-Air.fios-router.home>
2020-07-29 00:01:48 -0400

    finished work

--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,37 @@
 package com.stayready;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Problem03Test {
+
+    @Test
+    public void sample01Test(){
+        //Given
+        String queue = "2 1 5 3 4";
+        Problem03 prob3 = new Problem03(queue);
+        String expected = "3";
+
+        //When
+        String actual = prob3.minimumBrides();
+        System.out.println(actual);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample02Test(){
+        //Given
+        String queue = "2 5 1 3 4";
+        Problem03 prob3 = new Problem03(queue);
+        String expected = "Too chaotic";
+
+        //When
+        String actual = prob3.minimumBrides();
+        System.out.println(actual);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
