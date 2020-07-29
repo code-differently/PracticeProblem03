6c38acaaa6e8de3facc479acde58cb2ee1f8a6d6
Brent Oliver <balexanderoliver@gmail.com>
2020-07-28 20:46:11 -0400

    Completed Problem03, ready for review

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,34 @@
 package com.stayready;
 
 public class Problem03 {
+    // Need variable for holding total bribes
+    // More than two bribes, return Too chaotic
+
+    // Step 1: Start from beginning of array and iterate through the array
+    // Step 2: Determine distance from original place (moving backward in line, forward in array)
+    // Step 3: If current element is more than two places from its original place return Too Chaotic
+    // Step 4: For each distance from original place (not more than two) than add distance to count of bribes
+    // Step 5: Increase incrementer by number of bribes to skip over
+    // Step 5: Return the count
+
+    // 2 3 1 5 4  - 3
+    // 2 1 3 5 4  - 2
+    // 1 2 3 5 4  - 1
+    // 1 2 3 4 5
+
+    public String minimumBribes(int[] q) {
+        int minBribes = 0;
+
+        for(int i = 0; i < q.length; i++) {
+            int bribes = q[i] - (i + 1);
+            if(bribes > 0 && bribes <= 2) {
+                minBribes += bribes;
+            } else if(bribes > 2) {
+                return "Too Chaotic";
+            }
+        }
+
+        return minBribes + "";
+    }
+
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,67 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem03Test {
+
+    private static final int[] input1 = {2, 1, 5, 3, 4};
+    private static final int[] input2 = {2, 5, 1, 3, 4};
+    private static final int[] input3 = {2, 3, 1, 5, 4};
+    private static final int[] input4 = {3, 1, 2, 5, 6, 4, 8, 7};
+
+    Problem03 p3;
+    @Before
+    public void initialize() {
+        p3 = new Problem03();
+    }
+
+    @Test
+    public void minimumBribesTest1() {
+        // Given
+        String expected = "3";
+
+        // When
+        String actual = p3.minimumBribes(input1);
+
+        //
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minimumBribesTest2() {
+        // Given
+        String expected = "Too Chaotic";
+
+        // When
+        String actual = p3.minimumBribes(input2);
+
+        //
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minimumBribesTest3() {
+        // Given
+        String expected = "3";
+
+        // When
+        String actual = p3.minimumBribes(input3);
+
+        //
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minimumBribesTest4() {
+        // Given
+        String expected = "5";
+
+        // When
+        String actual = p3.minimumBribes(input4);
+
+        //
+        Assert.assertEquals(expected, actual);
+    }
 }
