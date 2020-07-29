82d250e0491726129f1361f794b970c24d3c9a6c
brianawilliams <williamsb16@comcast.net>
2020-07-29 07:49:26 -0400

    done

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,31 @@
 package com.stayready;
+import java.util.Arrays;
 
 public class Problem03 {
+
+    public String minimumBribe(String input){
+        int swapCount = 0;
+        int [] intArray = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i < intArray.length-1; i++) {
+            for (int j = 0; j < intArray.length- i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+
+                    swapCount++;
+                }
+            }
+        }
+        if(swapCount > 3){
+            return "Too Chaotic";
+        }else{
+            return String.valueOf(swapCount);
+        }
+
+
+
+    }
+
+
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,38 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem03Test {
+
+    @Test
+    public void minimumBrideTest01(){
+        //Given
+        Problem03 problem03 = new Problem03();
+        String input = "2 1 5 3 4";
+        String expected = "3";
+
+        //When
+        String actual = problem03.minimumBribe(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void minimumBrideTest02(){
+        //Given
+        Problem03 problem03 = new Problem03();
+        String input = "2 5 1 3 4";
+        String  expected = "Too Chaotic";
+
+        //When
+        String actual = problem03.minimumBribe(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
