6cb064c2dab54ff5b6c717cd3035dcf7331f8238
johndacosmo <jpali@bellsouth.net>
2020-07-29 01:56:31 -0400

    attempted

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,84 @@
 package com.stayready;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem03 {
+    private Integer [] q;
+    private int bribes;
+
+
+    public static void main(String [] args) {
+
+        String queue = "2 1 5 3 4";
+        Problem03 test = new Problem03(queue);
+        System.out.println(test.minimumBrides());
+    }
+
+    private void toIntArry(String x) {
+
+        String[] nums = x.split(" ");
+        q = new Integer[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+
+            q[i] = Integer.parseInt(nums[i]);
+        }
+
+    }
+
+    public Problem03(String x) {
+        toIntArry(x);
+        bribes = 0;
+    }
+
+
+    public String minimumBrides() {
+
+        String ans = "";
+        Integer n = 0, len = 0;
+
+        len = q.length;
+        for (int i = len; i > 1; i--) {
+
+            if( (i - 3 > 0) ) {
+
+                if(i == q[(len/2)]){
+
+                    bribes += 2;
+                }else if (i == q[(len/2) + bribes]) {
+
+                    bribes++;
+
+                } else if(i == q[i-1]) {
+
+
+                } else {
+
+                    return "Too Chaotic";
+                }
+
+            } else {
+
+                if ( i == q[i-1]) {
+
+                    bribes ++;
+
+                }
+
+            }
+
+        }
+
+        ans += bribes;
+        return ans;
+    }
+
+    public Integer[] getQ() {
+
+        return q;
+    }
+
+
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,46 @@
 package com.stayready;
 
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Problem03Test {
+
+    Problem03 prob3;
+    String queue;
+    String expected;
+    String actual;
+    @Before
+    public void setUp() {
+        queue = "";
+        expected = "";
+        actual = "";
+        
+    }
+
+
+    @Test
+    public void sample01Test(){
+
+        queue = "2 1 5 3 4";
+        prob3 = new Problem03(queue);
+        expected = "3";
+
+        actual = prob3.minimumBrides();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void sample02Test(){
+        //Given
+        String queue = "2 5 1 3 4";
+        Problem03 prob3 = new Problem03(queue);
+        String expected = "Too Chaotic";
+        //When
+        String actual = prob3.minimumBrides();
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
