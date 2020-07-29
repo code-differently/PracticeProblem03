08a58c818b987a6d06be88862136d7d114943a8a
Angela <asession001@gmail.com>
2020-07-28 22:56:35 -0400

    Finished

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,46 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem03 {
+    /*
+        1. Have a sorted array to compare the input array to
+        2. Compare from the sorted array end to the current value in input array
+        3. If the current position is greater than 2, return "Too Chaotic"
+        4. If the current positon is less than 2, count the swap
+    */
+
+ /*  int ans = 0; //bribes
+    for(int i=q.length-1;i>=0;i--){//for loop starts at the end of the queue and back tracks
+        int ch_pos = q[i]-(i+1);//2-1=1, number of bribes
+        if(ch_pos>2) { //if no. of bribes is greater than 2 then
+            System.out.println("Too chaotic");
+        }
+        else{
+            int st = Math.max(0,q[i]-2);//the max of the params is outputted
+            for(int j=st;j<i;j++){//j=max
+                if(q[j]>q[i]) ans++;
+            }
+        }
+
+  */
+
+    public String isChaotic(int [] queue){
+        Integer bribes = 0;
+
+        for(int i =queue.length-1; i>=0; i-- ){
+            int currentPosition = queue[i] - (i+1);
+            if(currentPosition >2){
+                return "Too Chaotic";
+            }else{
+              int swaps = Math.max(0, queue[i]-2);
+              for(int j=swaps; j<i; j++){
+                  if(queue[j] > queue[i]){
+                      bribes++;
+                  }
+              }
+            }
+        }
+        return bribes.toString();
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,63 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem03Test {
+
+    int [] queue1 = {2, 1, 5, 3, 4};
+    int [] queue2 = {2, 5, 1, 3, 4};
+    int [] queue3 = {1, 1, 4, 3, 6, 5, 7, 8, 9};
+    int [] queue4 = {1, 4, 5, 3, 8, 6, 7};
+    int [] queue5 = {3, 1, 2, 4, 5, 6, 8, 7, 9, 12, 11, 10};
+
+    @Test
+    public void isChaoticTest1(){
+        Problem03 problem03 = new Problem03();
+
+        String expected = "3";
+        String actual = problem03.isChaotic(queue1);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isChaoticTest2(){
+        Problem03 problem03 = new Problem03();
+
+        String expected = "Too Chaotic";
+        String actual = problem03.isChaotic(queue2);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isChaoticTest3(){
+        Problem03 problem03 = new Problem03();
+
+        String expected = "2";
+        String actual = problem03.isChaotic(queue3);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isChaoticTest4(){
+        Problem03 problem03 = new Problem03();
+
+        String expected = "Too Chaotic";
+        String actual = problem03.isChaotic(queue4);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isChaoticTest5(){
+        Problem03 problem03 = new Problem03();
+
+        String expected = "3";
+        String actual = problem03.isChaotic(queue5);
+
+        Assert.assertEquals(expected,actual);
+    }
 }
