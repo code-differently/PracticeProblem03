29311c6ab5b351dcade07a30df662db5811381e6
brianawilliams <williamsb16@comcast.net>
2020-07-29 10:16:09 -0400

    fixed some lines of code

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,28 +1,35 @@
 package com.stayready;
 import java.util.Arrays;
+import java.util.HashMap;
+
 
 public class Problem03 {
 
-    public String minimumBribe(String input){
+    public String minimumBribe(String input) {
         int swapCount = 0;
-        int [] intArray = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-        for(int i = 0; i < intArray.length-1; i++) {
-            for (int j = 0; j < intArray.length- i - 1; j++) {
+        HashMap<Integer, Integer> rollerCoasterRideLine = new HashMap<>();
+        //converting input into a integer array
+        int[] intArray = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int a = 0; a < intArray.length; a++) {
+            rollerCoasterRideLine.put(intArray[a], 2);
+        }
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length - i - 1; j++) {
                 if (intArray[j] > intArray[j + 1]) {
                     int temp = intArray[j];
                     intArray[j] = intArray[j + 1];
                     intArray[j + 1] = temp;
-
                     swapCount++;
+                    int secondPersonSwap = rollerCoasterRideLine.get(intArray[j + 1]) - 1;
+                    rollerCoasterRideLine.put(intArray[j + 1], secondPersonSwap);
                 }
             }
         }
-        if(swapCount > 3){
+        if (rollerCoasterRideLine.containsValue(-1) || rollerCoasterRideLine.containsValue(-2)) {
             return "Too Chaotic";
-        }else{
+        } else {
             return String.valueOf(swapCount);
         }
-
 
 
     }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -35,4 +35,41 @@ public class Problem03Test {
 
     }
 
+    @Test
+    public void minimumBrideTest03(){
+        //Given
+        Problem03 problem03 = new Problem03();
+        String input = "5 1 2 3 7 8 6 4";
+        String  expected = "Too Chaotic";
+
+        //When
+        String actual = problem03.minimumBribe(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void minimumBrideTest07(){
+        //Given
+        Problem03 problem03 = new Problem03();
+        String input = "1 2 5 3 7 8 6 4";
+
+
+        String  expected = "7";
+
+        //When
+        String actual = problem03.minimumBribe(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
+
+
+
+
 }
