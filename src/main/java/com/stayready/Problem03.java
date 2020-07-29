6139de1f95a475a8683733d2a27ef9d60c058b41
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-07-28 23:59:46 -0400

    Completed problem

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,46 @@
 package com.stayready;
 
-public class Problem03 {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Problem03
+{
+    public static Integer minimumBribes( Integer [] array )
+    {
+        Integer swaps = 0;
+        Integer length = array.length;
+
+        for (int i = length - 1; i >= 0; i--)
+        {
+            if(array[i] != (i + 1))
+            {
+                if(((i - 1) >= 0) && array[i - 1] == (i + 1))
+                {
+                    swaps ++;
+                    swap(array, i, (i - 1));
+                }
+                else if(((i - 2) >= 0) && array[i - 2] == (i + 1))
+                {
+                    swaps += 2;
+                    swap(array, (i - 2), (i - 1));
+                    swap(array, (i - 1), i);
+                }
+                else
+                {
+                    System.out.println("Too chaotic");
+                    break;
+                }
+            }
+        }
+        return swaps;
+    }
+
+    public static void swap( Integer [] array, Integer a, Integer b)//function to swap 2 elements
+    {   
+        List<Integer> targetList = Arrays.asList(array);            //array --> list
+        Collections.swap(targetList, a, b);                         //swap elements at given index
+        array = targetList.toArray(new Integer[targetList.size()]); //list  --> array
+
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,90 @@
 package com.stayready;
 
-public class Problem03Test {
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Problem03Test
+{   
+    Problem03 prob = new Problem03();
+
+    @Test
+    public void swapTest1()
+    {   
+        //given 
+        Integer [] array = { 4, 65, 8, 23 };
+        Integer  a = 2;
+        Integer b = 1;
+
+        //when
+        Problem03.swap(array, a, b);
+        Integer [] expected = { 4, 8, 65, 23 };
+        Integer [] actual = array;
+
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void swapTest2()
+    {   
+        //given 
+        Integer [] array = { 1, 2, 5, 4, 3 };
+        Integer  a = 2;
+        Integer b = 4;
+
+        //when
+        Problem03.swap(array, a, b);
+        Integer [] expected = { 1, 2, 3, 4, 5 };
+        Integer [] actual = array;
+
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void swapTest3()
+    {   
+        //given 
+        Integer [] array = { 95, 99, 98, 97, 96, 100 };
+        Integer  a = 0;
+        Integer b = (array.length - 1);
+
+        //when
+        Problem03.swap(array, a, b);
+        Integer [] expected = { 100, 99, 98, 97, 96, 95 };
+        Integer [] actual = array;
+
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void minBribesTest1()
+    {   
+        //given 
+        Integer [] array = { 2, 1, 5, 3, 4};
+
+        //when
+        Integer expected = 3;
+        Integer actual = Problem03.minimumBribes(array);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minBribesTest2()
+    {   
+        //given 
+        Integer [] array = { 2, 5, 1, 3, 4};
+
+        //when
+        Integer expected = 0;                           //returns 0 swaps, prints "Too chaotic"
+        Integer actual = Problem03.minimumBribes(array);
+
+        //then
+        assertEquals(expected, actual);
+    }
 }
