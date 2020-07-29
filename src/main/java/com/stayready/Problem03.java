98f203cde6dfc2659b1612b9a7d597314c202650
Francisco Burgos <burgfa@farmingdale.edu>
2020-07-29 10:01:19 -0400

    Ready for review

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,27 @@
 package com.stayready;
 
+import java.util.*;
+ /*
+/ Use the Queue interface
+/ Use PriorityQueue
+/ sort the arr in ascending order
+/ number with the highest value has the priority to move first
+/ the number with the highest value can only move twice
+/ then the following highest number has the priority to move next
+  */
 public class Problem03 {
+
+    public String entanglement(Integer[] arrInteger){
+        // sort the in ascending order
+        Arrays.sort(arrInteger);
+
+        Queue<Integer> queue = new PriorityQueue<>(Arrays.asList(arrInteger));
+
+        String order = "";
+        // starts fom the end
+        for(int index = arrInteger.length-1;index >= 0; index--){
+            order += queue.poll();
+        }
+        return order;
+    }
 }
