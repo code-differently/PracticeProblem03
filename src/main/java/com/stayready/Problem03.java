22fcf4443e7f623f54378116fb8c05d5c4ad94ef
stampedsteve <67400828+stampedsteve@users.noreply.github.com>
2020-07-29 02:50:28 -0400

    push it

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,37 @@
 package com.stayready;
 
 public class Problem03 {
+    
+
+    public int findBribeCount(int[] queue){
+        int firstChoice = 1;
+        int secondChoice = 2;
+        int thirdChoice = 3;
+
+        int bribeCount = 0;
+        //
+        for(int i =0; i<queue.length; i++){
+            if(queue[i] == firstChoice){
+                firstChoice = secondChoice;
+                secondChoice = thirdChoice;
+                thirdChoice++;
+            }
+            else if(queue[i] == secondChoice){
+                secondChoice = thirdChoice;
+                thirdChoice++;
+                bribeCount++;
+            }
+            else if(queue[i] == thirdChoice){
+                thirdChoice++;
+                bribeCount+=2;
+            }
+            else{
+                System.out.println("Too chaotic");
+                return 0;
+            }
+        }
+        
+
+        return bribeCount;
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,57 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem03Test {
+
+   
+    @Test
+    public void queueTest(){
+        //Given
+        Problem03 prac = new Problem03();
+        int[] arr = {2,1,5,3,4};
+        //When
+        int actual = prac.findBribeCount(arr);
+        int expected = 3;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void queueTest1(){
+        //Given
+        Problem03 prac = new Problem03();
+        int[] arr = {1,2,5,3,7,8,6,4};
+        //When
+        int actual = prac.findBribeCount(arr);
+        int expected = 7;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void queueTest2(){
+        //Given
+        Problem03 prac = new Problem03();
+        int[] arr = {1,2,5,3,4,7,8,6};
+        //When
+        int actual = prac.findBribeCount(arr);
+        int expected = 4;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void queueTest3(){
+        //Given
+        Problem03 prac = new Problem03();
+        int[] arr = {2,5,1,3,4};
+        //When
+        int actual = prac.findBribeCount(arr);
+        int expected = 0;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
