0d7a03ae6ce86f7f632ac307d2208cb73dae2447
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-07-29 10:42:05 -0400

    finished

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,42 @@
 package com.stayready;
 
 public class Problem03 {
+
+    public int minimumBribes(int[] intArr){
+        if(isChaotic(intArr)){
+            System.out.println("Too Chaotic");
+            return 0;  //returns 0, since it is too chaotic
+        }
+
+        int swaps = getNumSwaps(intArr);
+        return swaps;
+    }
+
+    public int getNumSwaps(int[] arr){
+        int numSwaps = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int current = arr[j];
+                    
+                    arr[j] = arr[i];
+                    arr[i] = current;
+
+                    numSwaps++;
+                }
+            }
+        }
+
+        return numSwaps;
+    }
+
+    public boolean isChaotic(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            if((arr[i] - i - 1) > 2)
+                return true;
+        }
+
+        return false;
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,60 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem03Test {
+
+    @Test
+    public void minimumBribesTest1(){
+        Problem03 p3 = new Problem03();
+        int[] arr = {2, 1, 5, 3, 4};
+
+        int actual = p3.minimumBribes(arr);
+        int expected = 3;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minimumBribesTest2(){
+        Problem03 p3 = new Problem03();
+        int[] arr = {2, 5, 1, 3, 4};
+
+        int actual = p3.minimumBribes(arr);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isChaoticTest1(){
+        Problem03 p3 = new Problem03();
+        int[] arr = {2, 5, 1, 3, 4};
+
+        boolean actual = p3.isChaotic(arr);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void isChaoticTest2(){
+        Problem03 p3 = new Problem03();
+        int[] arr = {2, 1, 5, 3, 4};
+
+        boolean actual = p3.isChaotic(arr);
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void getNumSwapsTest(){
+        Problem03 p3 = new Problem03();
+        int[] arr = {2, 1, 5, 3, 4};
+
+        int actual = p3.getNumSwaps(arr);
+        int expected = 3;
+
+        Assert.assertEquals(expected, actual);
+    }
 }
