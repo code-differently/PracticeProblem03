32ac897416be98181dc7de99870fea1a3d66a089
Makhya Wilridge <makhyaw@makhyas-air.myfiosgateway.com>
2020-07-28 22:55:38 -0400

    done.

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,23 @@
 package com.stayready;
 
+import java.util.Arrays;
 public class Problem03 {
+
+    public static String minimumBribes(String q) {
+        int[] arr = Arrays.stream(q.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int swaps = 0;
+        for(int i = (arr.length-1); i >= 0; i--) { // for loop starting at the last digit of the array, stopping when greater then or equal to 0 while running,decrementing by 1 in the loop
+            if(Integer.valueOf(arr[i])-i-1 > 2) {
+                return("Too Chaotic");
+            }
+            int k = (Integer.valueOf(arr[i])-2 > 0) ? (Integer.valueOf(arr[i])-2) : 0;
+            for (int j = k; j < i; j++) {
+                if (Integer.valueOf(arr[j]) > Integer.valueOf(arr[i])) {
+                    swaps++;
+                }
+            }
+        }
+        return String.valueOf(swaps);
+    }
 }
