8734e0b3cbab95d475f77cb0e9f262cd63d12ed2
Paola <camachopaola311@gmail.com>
2020-07-28 23:07:23 -0400

    Problem 03

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,42 @@
 package com.stayready;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Problem03 {
+    public String minimumBribes(int[] queue) {
+        String output = "";
+        int swaps = 0;
+            for (int i = queue.length - 1; i >= 0; i--) {
+                /*
+                if element at index i minus i plus 1
+                or if element jumped more than 2 spaces its too chaotic
+                */
+                if (queue[i] - (i + 1) > 2) {
+                    output = "Too chaotic!";
+                }
+                /*
+                else it goes into the for loop
+                j uses max which checks to make sure as i is decrementing that it is larger than 0
+                if it is j is then made the max
+                 */
+                for (int j = Math.max(0, queue[i] - 2); j < i; j++) {
+                    //if element at index j is greater than element of index i
+                    //that means a bribe has been made and a swap must be incremented
+                    if(queue[j] > queue[i]){
+                        swaps++;
+                    }
+                }
+            }
+            /*
+            checking to see if there were no chaotic elements skipping too far ahead
+            if not then output equals the number of swaps made converted into a string
+             */
+            if(output.length() == 0) {
+                output = Integer.toString(swaps);
+            }
+        return output;
+    }
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,70 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Problem03Test {
+    @Test
+    public void minimumBribesTest(){
+        //Given
+        String expected = "3";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{2, 1, 5, 3, 4});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void minimumBribesTest2(){
+        //Given
+        String expected = "Too chaotic!";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{2, 5, 1, 3, 4});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void minimumBribesTest3(){
+        //Given
+        String expected = "4";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{2, 1, 5, 4, 3});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void minimumBribesTest4(){
+        //Given
+        String expected = "Too chaotic!";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{5, 4, 3, 2, 1});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void minimumBribesTest5(){
+        //Given
+        String expected = "Too chaotic!";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{1, 5, 3, 2, 4});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void minimumBribesTest6(){
+        //Given
+        String expected = "6";
+        //When
+        Problem03 problem = new Problem03();
+        String actual = problem.minimumBribes(new int[]{3, 4, 5, 1, 2});
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
 }
